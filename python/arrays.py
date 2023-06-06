9ddb044127b1d4b279ee2d58960645133a906486
Ewerton Gonçalves <tonpublic97@gmail.com>
2023-06-06 14:16:21 -0300

    Correção de loops + Alteração Menu/

---
+++ python/arrays.py
@@ -0,0 +1,147 @@
+produtores = [
+    {
+    "codigoProdutor": 1,
+    "nome": "Agrotech",
+    "tipo": "Pessoa Juridica",
+    "areaPlantio": 1000,
+    "nomeFantasia": "Agrotech Enterprise",
+    "email": "contato@agrotech.com.br",
+    "telefone": "11 99999-9999"
+    },
+    {
+    "codigoProdutor": 2,
+    "nome": "Fazenda Feliz",
+    "tipo": "Pessoa Juridica",
+    "areaPlantio": 500,
+    "nomeFantasia": "Fazenda Feliz",
+    "email": "contato@fazendafeliz.com.br",
+    "telefone": "21 88888-8888"
+    },
+    {
+    "codigoProdutor": 3,
+    "nome": "Sítio Sol Nascente",
+    "tipo": "Pessoa Física",
+    "areaPlantio": 150,
+    "nomeFantasia": "",
+    "email": "contato@sitionsolnascente.com.br",
+    "telefone": "31 33333-3333"
+    },
+    {
+    "codigoProdutor": 4,
+    "nome": "Fazenda São João",
+    "tipo": "Pessoa Jurídica",
+    "areaPlantio": 800,
+    "nomeFantasia": "São João",
+    "email": "contato@fazendasaonjoao.com.br",
+    "telefone": "81 44444-4444"
+    },
+    {
+    "codigoProdutor": 5,
+    "nome": "Agricultura Orgânica Ltda.",
+    "tipo": "Pessoa Jurídica",
+    "areaPlantio": 300,
+    "nomeFantasia": "Orgânica",
+    "email": "contato@agriculturaorganica.com.br",
+    "telefone": "21 55555-5555"
+    }
+
+
+]
+empresas = [
+    {
+    "codigoEmpresa": 1,
+    "nome": "HealthTech",
+    "tipo": "Pessoa Juridica",
+    "areaPlantio": "N/Possui",
+    "nomeFantasia": "HealthTech Solutions",
+    "email": "contato@healthtech.com.br",
+    "telefone": "11 88888-8888"
+    },
+    {
+    "codigoEmpresa": 2,
+    "nome": "EcoTech",
+    "tipo": "Pessoa Juridica",
+    "areaPlantio": "N/Possui",
+    "nomeFantasia": "EcoTech Innovations",
+    "email": "contato@ecotech.com.br",
+    "telefone": "11 77777-7777"
+    },
+    {
+    "codigoEmpresa": 3,
+    "nome": "FinTech",
+    "tipo": "Pessoa Juridica",
+    "areaPlantio": "N/Possui",
+    "nomeFantasia": "FinTech Global",
+    "email": "contato@fintech.com.br",
+    "telefone": "11 66666-6666"
+    },
+    {
+    "codigoEmpresa": 4,
+    "nome": "AutoTech",
+    "tipo": "Pessoa Juridica",
+    "areaPlantio": "N/Possui",
+    "nomeFantasia": "AutoTech Motors",
+    "email": "contato@autotech.com.br",
+    "telefone": "11 55555-5555"
+    },
+    {
+    "codigoEmpresa": 5,
+    "nome": "CleanTech",
+    "tipo": "Pessoa Juridica",
+    "areaPlantio": "N/Possui",
+    "nomeFantasia": "CleanTech Industries",
+    "email": "contato@cleantech.com.br",
+    "telefone": "11 44444-4444"
+    }
+
+]
+servicos = [
+    {
+    "codigoEmpresa": 1,
+    "nome": "Planta Sempre Saudável",
+    "descricao": "A HealthTech é uma empresa de tecnologia que desenvolve soluções para o setor de saúde de plantas",
+    }
+]
+instituicoes = [
+    {
+    "codigoInstituicao": 1,
+    "nome": "Instituto So o Verde Salvara",
+    "tipo": "Pessoa Juridica",
+    "areaPlantio": "N/Possui",
+    "nomeFantasia": "So o Verde Salvara",
+    "email": "contato@sooversalvara.com.br",
+    "telefone": "11 44444-4444"
+    },
+    {
+    "codigoInstituicao": 2,
+    "nome": "Instituto Arvores Mais Fortes",
+    "tipo": "Pessoa Juridica",
+    "areaPlantio": "N/Possui",
+    "nomeFantasia": "Arvores Mais Fortes",
+    "email": "contato@arvoresmaisfortes.com.br",
+    "telefone": "11 55555-5555"
+    },
+    {
+    "codigoInstituicao": 3,
+    "nome": "Instituto Hortaliças maduras",
+    "tipo": "Pessoa Juridica",
+    "areaPlantio": "N/Possui",
+    "nomeFantasia": "Hortaliças maduras",
+    "email": "contato@ortalicasmaduras.com.br",
+    "telefone": "11 66666-6666"
+    },
+
+]
+projetos = [
+    {
+    "codigoProjeto": 1,
+    "nome": "Horta Comunitaria",
+    "descricao": "Projeto para a construção de uma horta comunitaria",
+    "Instituicao": "Instituto So o Verde Salvara"
+    }
+]
+
+users = [
+        {'usuario': 'agrotech', 'senha': '123', 'tipo': 'PR'},
+        {'usuario': 'admin', 'senha': '123', 'tipo': 'adm'},
+    ]
--- python/teste.py
+++
@@ -1,3 +0,0 @@
-from functions_app import buscar_produtor_por_id
-
-buscar_produtor_por_id(1)
